bf8c914dde52cabd590a3723527be207a0f50ef9
Yuhanyang30 <48681913+Yuhanyang30@users.noreply.github.com>
2019-07-13 17:48:49 +0800

    Update Shopping List.js

--- Shopping List.js
+++ Shopping List.js
@@ -40,3 +40,22 @@ input.addEventListener("keypress", addListAfterKeypress);
 function removeItem(evt){
 	evt.target.parentNode.remove();
 }
+
+//background color
+var css = document.querySelector("h3");
+var color1 = document.querySelector(".color1");
+var color2 = document.querySelector(".color2");
+var body = document.getElementById("gradient");
+
+
+// change background color
+function setGradient() {
+	body.style.background = 
+	"linear-gradient(to right,"
+	+ color1.value
+	+ ","
+	+ color2.value
+	+ ")";
+}
+color1.addEventListener("input", setGradient);
+color2.addEventListener("input", setGradient);
